9b8d488d7c8ddba7ded02a48e9a2fbc4c82302d7
Alegi <ale.giannetti@hotmail.it>
2019-03-18 17:06:07 +0100

    eliminato brush - aggiunto on click e double click pulizia codice

--- force_brush.js
+++ force_brush.js
@@ -1,22 +1,13 @@
-var chiavi;
+const forceSplit = d3.forceX(d => (d.group === '1') ? 200 : 700)
+    .strength(0.05);
 
-var dataSelection=[];
+var chiavi;
 
 var focus;
 
 var margin = {top: 5, right: 5, bottom: 5, left: 5},
-    margin2 = {top: 5, right: 5, bottom: 5, left: 5},
     width = 950 - margin.left - margin.right,
-    height = 800 - margin.top - margin.bottom,
-    height2 = 800 - margin2.top - margin2.bottom;
-
-var brush = d3.brushX()
-    .extent([[0, 0], [width, height2]])
-    .on("brush", brushed);
-
-var brushTot=d3.brush()
-    .extent([[0,0],[width, height]])
-    .on("end", selected);
+    height = 800 - margin.top - margin.bottom;
 
 var color= d3.scaleOrdinal(d3.schemeCategory10);
 
@@ -26,13 +17,15 @@ function drawgraph(data){
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom);
 
+
     var simulation = d3.forceSimulation()
         .force("link", d3.forceLink().id(function(d) { return d.id; }))
         .force("collide", d3.forceCollide().strength(2).radius(100))
         .force("charge", d3.forceManyBody().strength(-180).distanceMax(280))
         .force("xAxis",d3.forceX(width/2).strength(0.4))
         .force("yAxis", d3.forceY(height / 2).strength(0.8))
-        .force("center", d3.forceCenter(width / 2, height / 2));
+        .force("center", d3.forceCenter(width / 2, height / 2))
+    ;
 
     svg.append("defs").append("clipPath")
         .attr("id", "clip")
@@ -57,6 +50,14 @@ function drawgraph(data){
         .enter().append("circle")
         .attr("r", 15)
         .attr("opacity", "1")
+        .on("click", function () {
+            d3.select(this).style("fill", "lightcoral");
+            selected(d3.select(this));
+        })
+        .on('dblclick', function () {
+            d3.select(this).style("fill", "steelblue");
+            unselected(d3.select(this));
+        })
         .style("fill", function(d) {return color(d[chiavi[2]]); });
 
     var textElements = focus.append("g")
@@ -68,10 +69,6 @@ function drawgraph(data){
         .attr("font-size", 15)
         .attr("dx", 20)
         .attr("dy", 5);
-
-    svg.append("g")
-        .attr("class", "brushT")
-        .call(brushTot);
 
     node.append("title")
         .text(function(d) { return d.id; });
@@ -94,10 +91,6 @@ function drawgraph(data){
             .attr("x", function(d) { return d.x; })
             .attr("y", function(d) { return d.y; });
     }
-
-    focus.append("g")
-        .attr("class", "brush")
-        .call(brush);
 }
 function drawcpa(data){
     var margin = {top: 30, right: 100, bottom: 10, left: 80},
@@ -110,8 +103,7 @@ function drawcpa(data){
 
     var line = d3.line(),
         background,
-        foreground,
-        extents;
+        foreground;
 
     var svg = d3.select("#PCA").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -139,7 +131,6 @@ function drawcpa(data){
                 return +p[d]; }))
             .range(Range);
     }));
-    extents = dimensions.map(function(p) { return [0,0]; });
 
     // Add grey background lines for context.
     background = svg.append("g")
@@ -199,16 +190,6 @@ function drawcpa(data){
         .attr("y", -9)
         .text(function(d) { return d; });
 
-    /*  // Add and store a brush for each axis.
-      g.append("g")
-          .attr("class", "brush")
-          .each(function(d) {
-              d3.select(this).call(y[d].brush = d3.brushY().extent([[-8, 0], [8,height]]).on("brush start", brushstart).on("brush", brush_parallel_chart));
-          })
-          .selectAll("rect")
-          .attr("x", -8)
-          .attr("width", 16);*/
-
     function position(d) {
         var v = dragging[d];
         return v == null ? x(d) : v;
@@ -226,90 +207,37 @@ function drawcpa(data){
             return [position(p), y[p](d[p])]; }));
     }
 
-    /*    function brushstart() {
-            d3.event.sourceEvent.stopPropagation();
-        }*/
-
-// Handles a brush event, toggling the display of foreground lines.
-    /*function brush_parallel_chart() {
-    var domain;
-    var range;
-    for(var i=0;i<dimensions.length;++i){
-        if(d3.event.target==y[dimensions[i]].brush) {
-            range = y[dimensions[i]].range();
-            extents[i]=d3.event.selection.map(y[dimensions[i]].invert,y[dimensions[i]]);
-            // PROBLEMONE - ORDINAL SCALE NON HA LA FUNZIONE INVERT
-            // problemi nella selezione su asse y
-        }
-    }*/
-
-    /* foreground.style("display", function(d) {
-           return dimensions.every(function(p, i) {
-             if(extents[i][0]==0 && extents[i][0]==0) {
-                 return true;
-             }
-             console.log(extents[i]);
-
-             return extents[i][1] <= d[p] && d[p] <= extents[i][0];
-         }) ? null : "none";
-     });
- }*/
 }
 d3.json("miserables.json", function(error, data) {
     chiavi= d3.keys(data.links[0]);
     if (error) throw error;
     var l=data.links.length;
-    for (var i=0;i<l;i++) {
+    for (var i=0; i<l; i++) {
         data.links[i].id = i;
     }
     drawgraph(data);
     drawcpa(data);
 });
 
-function brushed() {
-    focus.selectAll("circle")
-        .attr("cx", function (d) {
-            return (d.x);
-        })
-        .attr("cy", function (d) {
-            return d.y;
-        });
-    focus.select(".axis--x").call(xAxis);
-}
-
-function selected(){
-    dataSelection = [];
-    var selection= d3.event.selection;
-    if (selection != null) {
-        focus.selectAll("circle")
-            .style("stroke-width", function (d) {
-                if ((d.x > selection[0][0]) && ((d.x) < selection[1][0]) && ((d.y) > selection[0][1]) && ((d.y) < selection[1][1])) {
-                    dataSelection.push(d.id);
-                    return "4";
-                } else {
-                    return "1";
-                }
-            });
-    }
-    else
-        {
-            focus.selectAll("circle")
-                .style("fill",function(d) {return color(d[chiavi[2]]); })
-                .style("stroke-width", "1")
-        }
-
-        d3.select("#PCA").selectAll(".forepath")
-            .style("stroke","steelblue");
+function selected(circle) {
 
     d3.select("#PCA").selectAll(".forepath")
-            .style("stroke",function(d){
-                if ((d.source.x > selection[0][0]) && (d.source.x < selection[1][0]) && (d.source.y > selection[0][1]) && (d.source.y < selection[1][1]) || (d.target.x > selection[0][0]) && (d.target.x < selection[1][0]) && (d.target.y > selection[0][1]) && (d.target.y < selection[1][1])) {
-                    dataSelection.push(d.id);
-                    return "red";
-                }
-                else
-                {
-                    return "steelblue";
-                }
-            });
+        .style("stroke",function(d){
+            if ((d.source.id == circle._groups[0][0].__data__.id)) {
+                dataSelection.push(d.id);
+                return "red";
+            }
+        })
+}
+
+function unselected(circle) {
+    d3.select("#PCA").selectAll(".forepath")
+        .style("stroke", function (d) {
+            console.log(circle._groups[0][0].__data__.id);
+            if ((d.source.id == circle._groups[0][0].__data__.id)) {
+                dataSelection.push(d.id);
+                return "steelblue";
+            }
+        })
+
 }
